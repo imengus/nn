e721e172af89357bbf26ef139019793cdc4ee2a2
Ilkin <imengusoglu@gmail.com>
2023-09-06 03:28:42 +0100

    added forward, backward prop etc

--- main.cpp
+++ main.cpp
@@ -1,35 +1,96 @@
 #include "matrix.h"
 #include <map>
-#include <vector>
 
 typedef map<int, Matrix> MatMap;
 using MatMap = map<int, Matrix>;
 using namespace std;
 
+class Network {
+    // private:
+    public:
+        vector<int> layer_dims{5, 4, 10};//{784, 100, 10};
+        vector<Matrix> activations;
+        vector<double> errors;
+        int depth = layer_dims.size();
+        double learn_rate = 0.01;
+        double relu_coeff = 0.5;
+        double w_sdev = 0.25;
+        double w_mean = 0.5;
+        double b_sdev = 0;
+        double b_mean = 0;
+        double mse = 0;
+        Matrix gradient;
+        Matrix Y_hat;
+        MatMap weights;
+        MatMap biases;
 
-void init_params(vector<int> layer_dims, MatMap &weights, MatMap &biases) {
-    int L = layer_dims.size();
+        void initialize_parameters() {
 
-    for (int i = 1; i < L; i++) {
-        Matrix temp_w(layer_dims[i-1], layer_dims[i], true);
-        temp_w = temp_w * 0.01;
-        Matrix temp_b(1, layer_dims[i]);
+            for (int i = 1; i < depth; i++) {
+                Matrix temp_w(layer_dims[i-1], layer_dims[i], w_sdev, w_mean);
+                Matrix temp_b(1, layer_dims[i], b_sdev, b_mean);
 
-        weights[i] = temp_w;
-        biases[i] = temp_b;
+                weights[i] = temp_w.relu(relu_coeff);
+                biases[i] = temp_b;
+            }
+        }
 
-    }
-}
+        void print_parameters() {
+            for (int i = 1; i < depth; i++ ) {
+                cout << "___weight " << i << endl;
+                weights[i].print();
+                cout << "___bias " << i << endl;
+                biases[i].print();
+                cout << endl;
+            }
+        }
+
+        void calculate_error(Matrix Y_hat, Matrix Y) {
+            mse = (Y_hat - Y).l2_normalized();
+            gradient = (Y_hat - Y) * 2;
+        }
+
+        void forward(Matrix input) {
+            Matrix activation = input;
+            for (int i = 1; i < depth; i++) {
+                activation = weights[i].mult(activation) + biases[i];
+                activations.push_back(activation);
+            }
+            Y_hat = activation;
+        }
+
+        void backward(Matrix Y) {
+            calculate_error(Y_hat, Y);
+            gradient = gradient * Y_hat.relu_derivative(0);
+            for (int i = depth-1; i > 0; i--) {
+                Matrix gradient_weight = gradient.transpose().mult(activations[i-1]);
+                Matrix gradient_bias = gradient * -1;
+
+                weights[i] = weights[i] - gradient_weight * learn_rate;
+                biases[i] = biases[i] - gradient_bias * learn_rate;
+
+                gradient = gradient.mult(weights[i]).transpose();
+
+            }
+        }
+};
+
+
+
 int main () {
-    MatMap weights;
-    MatMap biases;
-    init_params({5, 8, 1}, weights, biases);
-//     MatMap a = *p;
-//     for (int i = 0; i < 2; i++ ) {
-//         for (int j = 1; j < 4; j++ ) {
-//             cout << "___" << j << endl;
-//             (*(p + i))[j].print();
-//         }
-//    }
+    Network nn;
+    nn.initialize_parameters();
+    // nn.print_parameters();
+    Matrix input(5, 1);
+    // vector<vector<double>> temp{{1., 2., 3., 4., 5.}};
+    input.mat = {{1, 2, 3, 4, 5}};
+    nn.forward(input.transpose());
+    nn.Y_hat.print();
+    // input.transpose().print();
+    // Matrix first(5, 5);
+    // Matrix res = first * -1;
+    // first.relu_derivative(0.0).print();
+    // cout << endl;
+    // res.relu_derivative(0.0).print();
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -2,65 +2,60 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
+
+#define IterateMatrix(EXPRESSION) {\
+    for (int i = 0; i < height; ++i) {\
+        for (int j = 0; j < width; ++j) {\
+            EXPRESSION\
+        }}}
+    
+#define Overload(op) \
+    Matrix operator op (const Matrix& other) const {\
+        if (width != other.width || height != other.height) {\
+            throw invalid_argument("Matrix dimensions must match");}\
+        \
+        Matrix result(width, height);\
+        IterateMatrix(result.mat[i][j] = mat[i][j] op other.mat[i][j];);\
+        return result;\
+        }
 
 class Matrix {
     public:
         int width;
         int height;
         vector<vector<double>> mat;
-        Matrix(int wid=0, int hei=0, bool rand_fill=false, double fill=0.0) : width(wid), height(hei) {
-            mat.resize(height, vector<double>(width, fill));
-            if (rand_fill) {
+        Matrix(int wid=0, int hei=0, double std_dev=1, double mean=0.0) : width(wid), height(hei) {
+            mat.resize(height, vector<double>(width, mean));
+            if (std_dev) {
                 
                 default_random_engine generator;
-                normal_distribution<double> distribution(0, 1);
+                normal_distribution<double> distribution(mean, std_dev);
 
-                for (int i = 0; i < height; ++i) {
-                    for (int j = 0; j < width; ++j) {
-                        mat[i][j] = distribution(generator);
-                    }
-                }
+                IterateMatrix(mat[i][j] = distribution(generator););
             }
-            // cout << "Created" << endl;
         }
-        ~Matrix() {}//cout << "Destroyed " << endl; }
+        ~Matrix() {}
 
+        // General Operations
+        // ------------------
         Matrix transpose() const {
-            Matrix trans(width, height);
-
-            for (int i = 0; i < height; ++i) {
-                for (int j = 0; j < width; ++j) {
-                    trans.mat[i][j] = mat[j][i];
-                }
-            }
+            Matrix trans(height, width);
+            IterateMatrix(trans.mat[j][i] = mat[i][j];);
             return trans;
         }
 
         Matrix operator*(const double scalar) const {
             Matrix result(width, height);
-            for (int i = 0; i < height; ++i) {
-                for (int j = 0; j < width; ++j) {
-                    result.mat[i][j] = mat[i][j] * scalar;
-                }
-            }
+            IterateMatrix(result.mat[i][j] = mat[i][j] * scalar;);
             return result;
         }
 
-
-        Matrix operator+(const Matrix& other) const {
-            if (width != other.width || height != other.height) {
-                throw invalid_argument("Matrix dimensions must match for addition");
-            }
-
-            Matrix result(width, height);
-            for (int i = 0; i < height; ++i) {
-                for (int j = 0; j < width; ++j) {
-                    result.mat[i][j] = mat[i][j] + other.mat[i][j];
-                }
-            }
-            return result;
-        }
+        Overload(+);
+        Overload(-);
+        Overload(*);
+        Overload(/);
 
         Matrix mult(const Matrix& other) const {
             if (width != other.height) {
@@ -85,4 +80,31 @@ class Matrix {
                 cout << '\n';
             }
         }
+
+        // Machine Learning Functions
+        // --------------------------
+        Matrix relu(double coeff) const {
+            Matrix result(width, height);
+            IterateMatrix(result.mat[i][j] = (0<mat[i][j]) ? mat[i][j] : (coeff * mat[i][j]););
+            return result;
+        }
+
+        Matrix relu_derivative(double coeff) const {
+            Matrix result(width, height);
+            IterateMatrix(result.mat[i][j] = (0<=mat[i][j]) ? 1.0 : coeff;);
+            return result;
+        }
+
+        const double l1_normalized() const {
+            double result{};
+            IterateMatrix(result += abs(mat[i][j]););
+            return sqrt(result);
+        }
+
+        const double l2_normalized() const {
+            double result{};
+            IterateMatrix(result += pow(mat[i][j], 2););
+            return sqrt(result);
+        }
 };
+
